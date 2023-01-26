1f8736e7f2dc47d3ab151305af0cfa140bfb154c
kailashsriram <105009573+kailashsriram5@users.noreply.github.com>
2023-01-27 00:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def flat_list(l):
     # input nested list is stored in l
     # Insert Code below
     sdl=[]
-    
+    for items in l:
+        for i in items:
+            sdl.append(i)
     # Insert Code Above
     # Return single-dimension list.
     return sdl
